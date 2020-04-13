aad443c12847243db660e73ad62ef648677e6847
343695222 <40121447+343695222@users.noreply.github.com>
2020-04-14 02:34:08 +0800

    Add files via upload
    Solved the following problems：
    $ python test_background-matting_image.py -m /real-hand-held -i /sample_data/input/ -o /sample_data/output/ -tb /sample_data/background/0001.png
    Traceback (most recent call last):
      File "test_background-matting_image.py", line 20, in <module>
        print('CUDA Device: ' + os.environ["CUDA_VISIBLE_DEVICES"])
      File "D:\Anaconda3\envs\back-matting\lib\os.py", line 669, in __getitem__
        raise KeyError(key) from None
    KeyError: 'CUDA_VISIBLE_DEVICES'
    (back-matting)

--- test_background-matting_image.py
+++ test_background-matting_image.py
@@ -5,7 +5,7 @@ import os, glob, time, argparse, pdb, cv2
 #import matplotlib.pyplot as plt
 import numpy as np
 from skimage.measure import label
-
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 import torch
 import torch.nn as nn
@@ -16,7 +16,7 @@ from functions import *
 from networks import ResnetConditionHR
 
 torch.set_num_threads(1)
-#os.environ["CUDA_VISIBLE_DEVICES"]="4"
+
 print('CUDA Device: ' + os.environ["CUDA_VISIBLE_DEVICES"])
 
 
@@ -32,7 +32,7 @@ parser.add_argument('-b', '--back', type=str,default=None,help='Captured backgro
 args=parser.parse_args()
 
 #input model
-model_main_dir='Models/' + args.trained_model + '/';
+model_main_dir='Models1/' + args.trained_model + '/';
 #input data path
 data_path=args.input_dir
 
