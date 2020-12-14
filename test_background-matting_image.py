fe2b1c97c998e209540983af7c5f865a76229f65
Robert Krajewski <robert.w.krajewski@googlemail.com>
2020-12-14 19:06:40 +0100

    Remove unnecessary semicolon in test_background-matting_image.py

--- test_background-matting_image.py
+++ test_background-matting_image.py
@@ -29,7 +29,7 @@ parser.add_argument('-b', '--back', type=str, default=None,
 args = parser.parse_args()
 
 # input model
-model_main_dir = 'Models/' + args.trained_model + '/';
+model_main_dir = 'Models/' + args.trained_model + '/'
 # input data path
 data_path = args.input_dir
 
@@ -40,13 +40,13 @@ else:
     args.video = False
     print('Using image mode')
     # target background path
-    back_img10 = cv2.imread(args.target_back);
-    back_img10 = cv2.cvtColor(back_img10, cv2.COLOR_BGR2RGB);
+    back_img10 = cv2.imread(args.target_back)
+    back_img10 = cv2.cvtColor(back_img10, cv2.COLOR_BGR2RGB)
     # Green-screen background
-    back_img20 = np.zeros(back_img10.shape);
-    back_img20[..., 0] = 120;
-    back_img20[..., 1] = 255;
-    back_img20[..., 2] = 155;
+    back_img20 = np.zeros(back_img10.shape)
+    back_img20[..., 0] = 120
+    back_img20[..., 1] = 255
+    back_img20[..., 2] = 155
 
 # initialize network
 fo = glob.glob(model_main_dir + 'netG_epoch_*')
@@ -54,15 +54,15 @@ model_name1 = fo[0]
 netM = ResnetConditionHR(input_nc=(3, 3, 1, 4), output_nc=4, n_blocks1=7, n_blocks2=3)
 netM = nn.DataParallel(netM)
 netM.load_state_dict(torch.load(model_name1))
-netM.cuda();
+netM.cuda()
 netM.eval()
 cudnn.benchmark = True
 reso = (512, 512)  # input resolution to the network
 
 # load captured background for video mode, fixed camera
 if args.back is not None:
-    bg_im0 = cv2.imread(args.back);
-    bg_im0 = cv2.cvtColor(bg_im0, cv2.COLOR_BGR2RGB);
+    bg_im0 = cv2.imread(args.back)
+    bg_im0 = cv2.cvtColor(bg_im0, cv2.COLOR_BGR2RGB)
 
 # Create a list of test images
 test_imgs = [f for f in os.listdir(data_path) if
@@ -78,26 +78,26 @@ if not os.path.exists(result_path):
 for i in range(0, len(test_imgs)):
     filename = test_imgs[i]
     # original image
-    bgr_img = cv2.imread(os.path.join(data_path, filename));
-    bgr_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB);
+    bgr_img = cv2.imread(os.path.join(data_path, filename))
+    bgr_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
 
     if args.back is None:
         # captured background image
-        bg_im0 = cv2.imread(os.path.join(data_path, filename.replace('_img', '_back')));
-        bg_im0 = cv2.cvtColor(bg_im0, cv2.COLOR_BGR2RGB);
+        bg_im0 = cv2.imread(os.path.join(data_path, filename.replace('_img', '_back')))
+        bg_im0 = cv2.cvtColor(bg_im0, cv2.COLOR_BGR2RGB)
 
     # segmentation mask
-    rcnn = cv2.imread(os.path.join(data_path, filename.replace('_img', '_masksDL')), 0);
+    rcnn = cv2.imread(os.path.join(data_path, filename.replace('_img', '_masksDL')), 0)
 
     if args.video:  # if video mode, load target background frames
         # target background path
-        back_img10 = cv2.imread(os.path.join(args.target_back, filename.replace('_img.png', '.png')));
-        back_img10 = cv2.cvtColor(back_img10, cv2.COLOR_BGR2RGB);
+        back_img10 = cv2.imread(os.path.join(args.target_back, filename.replace('_img.png', '.png')))
+        back_img10 = cv2.cvtColor(back_img10, cv2.COLOR_BGR2RGB)
         # Green-screen background
-        back_img20 = np.zeros(back_img10.shape);
-        back_img20[..., 0] = 120;
-        back_img20[..., 1] = 255;
-        back_img20[..., 2] = 155;
+        back_img20 = np.zeros(back_img10.shape)
+        back_img20[..., 0] = 120
+        back_img20[..., 1] = 255
+        back_img20[..., 2] = 155
 
         # create multiple frames with adjoining frames
         gap = 20
@@ -110,35 +110,35 @@ for i in range(0, len(test_imgs)):
                 idx[t] = idx[t] - len(test_imgs)
 
             file_tmp = test_imgs[idx[t]]
-            bgr_img_mul = cv2.imread(os.path.join(data_path, file_tmp));
-            multi_fr_w[..., t] = cv2.cvtColor(bgr_img_mul, cv2.COLOR_BGR2GRAY);
+            bgr_img_mul = cv2.imread(os.path.join(data_path, file_tmp))
+            multi_fr_w[..., t] = cv2.cvtColor(bgr_img_mul, cv2.COLOR_BGR2GRAY)
 
     else:
         ## create the multi-frame
         multi_fr_w = np.zeros((bgr_img.shape[0], bgr_img.shape[1], 4))
-        multi_fr_w[..., 0] = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2GRAY);
+        multi_fr_w[..., 0] = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2GRAY)
         multi_fr_w[..., 1] = multi_fr_w[..., 0]
         multi_fr_w[..., 2] = multi_fr_w[..., 0]
         multi_fr_w[..., 3] = multi_fr_w[..., 0]
 
     # crop tightly
-    bgr_img0 = bgr_img;
+    bgr_img0 = bgr_img
     bbox = get_bbox(rcnn, R=bgr_img0.shape[0], C=bgr_img0.shape[1])
 
     crop_list = [bgr_img, bg_im0, rcnn, back_img10, back_img20, multi_fr_w]
     crop_list = crop_images(crop_list, reso, bbox)
-    bgr_img = crop_list[0];
-    bg_im = crop_list[1];
-    rcnn = crop_list[2];
-    back_img1 = crop_list[3];
-    back_img2 = crop_list[4];
+    bgr_img = crop_list[0]
+    bg_im = crop_list[1]
+    rcnn = crop_list[2]
+    back_img1 = crop_list[3]
+    back_img2 = crop_list[4]
     multi_fr = crop_list[5]
 
     # process segmentation mask
     kernel_er = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     kernel_dil = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    rcnn = rcnn.astype(np.float32) / 255;
-    rcnn[rcnn > 0.2] = 1;
+    rcnn = rcnn.astype(np.float32) / 255
+    rcnn[rcnn > 0.2] = 1
     K = 25
 
     zero_id = np.nonzero(np.sum(rcnn, axis=1) == 0)
@@ -155,13 +155,13 @@ for i in range(0, len(test_imgs)):
     rcnn = np.delete(rcnn, range(reso[0], reso[0] + K), 0)
 
     # convert to torch
-    img = torch.from_numpy(bgr_img.transpose((2, 0, 1))).unsqueeze(0);
+    img = torch.from_numpy(bgr_img.transpose((2, 0, 1))).unsqueeze(0)
     img = 2 * img.float().div(255) - 1
-    bg = torch.from_numpy(bg_im.transpose((2, 0, 1))).unsqueeze(0);
+    bg = torch.from_numpy(bg_im.transpose((2, 0, 1))).unsqueeze(0)
     bg = 2 * bg.float().div(255) - 1
-    rcnn_al = torch.from_numpy(rcnn).unsqueeze(0).unsqueeze(0);
+    rcnn_al = torch.from_numpy(rcnn).unsqueeze(0).unsqueeze(0)
     rcnn_al = 2 * rcnn_al.float().div(255) - 1
-    multi_fr = torch.from_numpy(multi_fr.transpose((2, 0, 1))).unsqueeze(0);
+    multi_fr = torch.from_numpy(multi_fr.transpose((2, 0, 1))).unsqueeze(0)
     multi_fr = 2 * multi_fr.float().div(255) - 1
 
     with torch.no_grad():
@@ -176,7 +176,7 @@ for i in range(0, len(test_imgs)):
         # for regions with alpha>0.95, simply use the image as fg
         fg_pred = img * al_mask + fg_pred_tmp * (1 - al_mask)
 
-        alpha_out = to_image(alpha_pred[0, ...]);
+        alpha_out = to_image(alpha_pred[0, ...])
 
         # refine alpha with connected component
         labels = label((alpha_out > 0.05).astype(int))
@@ -189,18 +189,18 @@ for i in range(0, len(test_imgs)):
 
         alpha_out = (255 * alpha_out[..., 0]).astype(np.uint8)
 
-        fg_out = to_image(fg_pred[0, ...]);
-        fg_out = fg_out * np.expand_dims((alpha_out.astype(float) / 255 > 0.01).astype(float), axis=2);
+        fg_out = to_image(fg_pred[0, ...])
+        fg_out = fg_out * np.expand_dims((alpha_out.astype(float) / 255 > 0.01).astype(float), axis=2)
         fg_out = (255 * fg_out).astype(np.uint8)
 
         # Uncrop
-        R0 = bgr_img0.shape[0];
+        R0 = bgr_img0.shape[0]
         C0 = bgr_img0.shape[1]
         alpha_out0 = uncrop(alpha_out, bbox, R0, C0)
         fg_out0 = uncrop(fg_out, bbox, R0, C0)
 
     # compose
-    back_img10 = cv2.resize(back_img10, (C0, R0));
+    back_img10 = cv2.resize(back_img10, (C0, R0))
     back_img20 = cv2.resize(back_img20, (C0, R0))
     comp_im_tr1 = composite4(fg_out0, back_img10, alpha_out0)
     comp_im_tr2 = composite4(fg_out0, back_img20, alpha_out0)
