79b0b290efa37602a4503ef2c29ff1bd79daf5aa
Robert Krajewski <robert.w.krajewski@googlemail.com>
2020-12-14 19:05:32 +0100

    Remove unnecessary imports

--- test_background-matting_image.py
+++ test_background-matting_image.py
@@ -1,11 +1,8 @@
 from __future__ import print_function
 
-import os, glob, time, argparse, pdb, cv2
-# import matplotlib.pyplot as plt
-import numpy as np
+import os, glob, argparse
 from skimage.measure import label
 
-import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.backends.cudnn as cudnn
